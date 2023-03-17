f80fccc9f36b796cbe806455de813b552fdf06c6
milesccoleman <milesccoleman@gmail.com>
2023-03-17 19:11:21 -0400

    started basic dashboard

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <PublicSpeakingDashboard msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import PublicSpeakingDashboard from './components/PublicSpeakingDashboard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PublicSpeakingDashboard
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/PublicSpeakingDashboard.vue
@@ -0,0 +1,135 @@
+<template>
+  <div class="dashboard">
+    <h1>{{ msg }}</h1>
+    <p id="timer">
+		{{ time }} <br>
+		Time
+    </p>
+    <p id="totalWords">
+		{{ totalWords }} <br>
+		Total Words Detected
+    </p>
+     <p id="wpm">
+		{{ wpm }} <br>
+		Overall Average Words Per Minute
+    </p>
+    <button v-on:click="initiateVoiceControl">Start</button>    
+    <p id="output">
+      {{ output }}
+    </p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'publicSpeakingDashboard',
+  props: {
+  }, 
+  data () {
+		return {
+			msg: 'Public Speaking Dashboard', 
+			wordsSpoken: '', 
+			output: 'Recognized Text',
+			initialTime: 0,  
+			time: 0,
+			timeElapsed: 0, 
+			timeDifference: 0, 
+			wordCount: 0,
+			totalWords: 0, 
+			wordCountDividedByTime: 0,
+			wpm: 0
+		}
+	}, 
+
+	methods: {
+	
+		initiateVoiceControl: function () {
+			console.log('voice recognition initiated')
+			window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+			let finalTranscript = '';
+			let recognition = new window.SpeechRecognition();
+			recognition.interimResults = true;
+			recognition.maxAlternatives = 10;
+			recognition.continuous = true;
+			recognition.onresult = (event) => {
+				let interimTranscript = '';
+				for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
+					let transcript = event.results[i][0].transcript;
+					if (event.results[i].isFinal) {
+						finalTranscript += transcript;
+					} else {
+						interimTranscript += transcript;
+					}
+				}
+				console.log("transcript: " + interimTranscript)
+				this.wordsSpoken = finalTranscript
+				this.output =  this.wordsSpoken += interimTranscript
+				this.wordCount = this.countWords(this.output)
+				this.totalWords = this.wordCount
+				}
+		this.initialTime = Date.now();
+		this.grabTime()
+		recognition.start()
+		window.setInterval(this.grabTime, 1000)
+		window.setInterval(this.registerWPM, 1000)
+		},
+	
+		grabTime: function () {
+			
+				
+					this.timeDifference = Date.now() - this.initialTime;
+					var formatted = convertTime(this.timeDifference);
+					document.getElementById('timer').innerHTML = '' + formatted;
+					this.timeElapsed = this.timeDifference
+					console.log("og time elapsed " + this.timeElapsed)
+					return this.timeElapsed
+				
+				function convertTime(miliseconds) {
+					var totalSeconds = Math.floor(miliseconds/1000);
+					var minutes = Math.floor(totalSeconds/60);
+					var seconds = totalSeconds - minutes * 60;
+					return minutes + ':' + seconds;
+				}
+		},
+		
+		
+		
+		countWords: function (str){
+			const arr = str.split(' ');
+			return arr.filter(word => word !== '').length;
+		}, 
+		
+		registerWPM: function () {
+		this.wpm = Math.round(this.wordCount/(this.timeElapsed/1000) * 60) 
+			console.log("time elapsed " + this.timeElapsed)	
+			console.log("word count " + this.wordCount)
+			
+			
+			
+		}
+	
+	
+	
+	
+	}//
+}//	
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
